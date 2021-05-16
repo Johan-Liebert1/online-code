a2779f01a628b946bb3ec92d74faf0ad82e773e6
Johan-Liebert1 <pragyanpoudyal41999@gmail.com>
2021-05-16 11:02:44 +0530

    refactor code

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -11,54 +11,65 @@ export const fetchPlugin = (inputCode: string) => {
 	return {
 		name: "fetchPlugin",
 		setup: (build: esbuild.PluginBuild) => {
+			build.onLoad({ filter: /(^index\.js$)/ }, () => {
+				return {
+					loader: "jsx",
+					contents: inputCode
+				};
+			});
+
 			build.onLoad({ filter: /.*/ }, async (args: any) => {
-				if (args.path === "index.js") {
-					return {
-						loader: "jsx",
-						contents: inputCode
-					};
-				}
-
 				/*
-            check to see if we've already stored the file in cache (indexDB).
-            If yes then just return it immediately, else, make a request and store
-            the data in indexDB  
-            */
-
+                check to see if we've already stored the file in cache (indexDB).
+                If yes then just return it immediately, else, make a request and store
+                the data in indexDB  
+                */
 				const cachedResult = await fileCahce.getItem<esbuild.OnLoadResult>(
 					args.path
 				);
 
 				if (cachedResult) {
 					return cachedResult;
-				} else {
-					const { data, request } = await axios.get(args.path);
-					// console.log("data = ", data, "request = ", request);
+				}
+			});
 
-					const fileType = args.path.match(/.css$/) ? "css" : "jsx";
+			build.onLoad({ filter: /.css$/ }, async (args: any) => {
+				const { data, request } = await axios.get(args.path);
+				// console.log("data = ", data, "request = ", request);
 
-					const contents =
-						fileType === "css"
-							? `
+				const contents = `
                                 const styles = document.createElement('style');
                                 styles.innerText = '${data
 									.replace(/\"/g, '\\"')
 									.replace(/\'/g, "\\'")
 									.replace(/\n/g, "")}';
                                 document.head.appendChild(styles);
-                            `
-							: data;
+                            `;
 
-					let result: esbuild.OnLoadResult = {
-						loader: "jsx",
-						contents,
-						resolveDir: new URL("./", request.responseURL).pathname
-					};
+				let result: esbuild.OnLoadResult = {
+					loader: "jsx",
+					contents,
+					resolveDir: new URL("./", request.responseURL).pathname
+				};
 
-					await fileCahce.setItem(args.path, result);
+				await fileCahce.setItem(args.path, result);
 
-					return result;
-				}
+				return result;
+			});
+
+			build.onLoad({ filter: /.*/ }, async (args: any) => {
+				const { data, request } = await axios.get(args.path);
+				// console.log("data = ", data, "request = ", request);
+
+				let result: esbuild.OnLoadResult = {
+					loader: "jsx",
+					contents: data,
+					resolveDir: new URL("./", request.responseURL).pathname
+				};
+
+				await fileCahce.setItem(args.path, result);
+
+				return result;
 			});
 		}
 	};
